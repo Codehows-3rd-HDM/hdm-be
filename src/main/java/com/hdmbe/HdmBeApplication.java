fa3bdf884ecd5be818d46929f5fa2388afdcb181
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-11-21 15:43:46 +0900

    백엔드 프로젝트 생성

---
+++ src/main/java/com/hdmbe/HdmBeApplication.java
@@ -0,0 +1,13 @@
+package com.hdmbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdmBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdmBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdmbe/HdmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdmbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdmBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
