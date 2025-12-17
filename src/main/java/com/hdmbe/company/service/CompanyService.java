6b172ea49cf7e551c59d5fa90b1b90f33f1cd957
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-12-17 14:59:49 +0900

    Update CompanyService.java

--- src/main/java/com/hdmbe/company/service/CompanyService.java
+++ src/main/java/com/hdmbe/company/service/CompanyService.java
@@ -3,6 +3,10 @@ package com.hdmbe.company.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
