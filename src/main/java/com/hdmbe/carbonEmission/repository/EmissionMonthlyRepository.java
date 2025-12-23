50077cb8ea37d2f1357ea23403259afb4fce8202
ghdtjrals702-svg <ghdtjrals702@gmail.com>
2025-12-23 14:56:28 +0900

    Create EmissionMonthlyRepository.java

--- src/main/java/com/hdmbe/carbonEmission/repository/EmissionMonthlyRepository.java
+++ src/main/java/com/hdmbe/carbonEmission/repository/EmissionMonthlyRepository.java
@@ -10,9 +10,11 @@ public interface EmissionMonthlyRepository extends JpaRepository<CarbonEmissionM
     // 기간으로 삭제
     // 특정 월 삭제
     void deleteByYearAndMonth(int year, int month);
+
     // 특정 연도 전체 삭제
     void deleteByYear(int year);
 
     // 차량, 연도, 월 조건으로 데이터 단건 조회
     Optional<CarbonEmissionMonthlyLog> findByVehicleAndYearAndMonth(Vehicle vehicle, int year, int month);
+
 }
