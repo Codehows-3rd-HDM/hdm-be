c19f69929150f0d9a57ae8abf1a3429cf20d1b98
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2026-01-07 11:45:13 +0900

    실행시 계정 생성 제거

--- src/main/java/com/hdmbe/HdmBeApplication.java
+++ src/main/java/com/hdmbe/HdmBeApplication.java
@@ -17,30 +17,4 @@ public class HdmBeApplication {
         SpringApplication.run(HdmBeApplication.class, args);
 
     }
-
-    // 애플리케이션 시작 시 실행되는 더미 데이터 생성
-    @Bean
-    public CommandLineRunner initDummyUsers(UserAccountService userAccountService) {
-        return args -> {
-            try {
-                UserAccountDto admin = new UserAccountDto();
-                admin.setUserName("hdm2518");
-                admin.setPassword("hdrd0104");
-                admin.setRole(Role.SUPERADMIN);
-                userAccountService.saveUserAccount(admin);
-
-                UserAccountDto admin1 = new UserAccountDto();
-                admin1.setUserName("1");
-                admin1.setPassword("1");
-                admin1.setRole(Role.SUPERADMIN);
-                userAccountService.saveUserAccount(admin1);
-
-                System.out.println("✅ 더미 계정 생성 완료");
-            } catch (Exception e) {
-                System.out.println("⚠️ 더미 계정 생성 중 오류: " + e.getMessage());
-            }
-        };
-    }
-
-
 }
